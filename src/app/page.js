adc5794d614370169f16f943fdf1aa5eeec0a0a6
Adenilton Henrique Ribeiro <gbi001@einstein.br>
2024-10-21 20:58:10 -0300

    Add h5 player

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Image from 'next/image';
+import AudioPlayer from '../components/H5Player/es';
+import '../components/H5Player/styles/styles.css';
 
 export default function Home() {
   return (
@@ -15,10 +17,11 @@ export default function Home() {
           justifyContent: 'center',
         }}
       >
-        <div>
+        <div style={{ display: 'flex', width: 500, justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
           <Image src="/uydlogo.png" width="300" height="300" alt="uyd logo" />
-          <br/>
-          #player
+          <AudioPlayer
+            src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+          />
         </div>
       </Box>
     </Container>
